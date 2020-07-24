b6944fc1f9a475a0ff912d8b9f8b5163b12557a7
Gustavo Souza <gtssouza94@gmail.com>
2020-07-23 22:27:19 -0300

    Salvando despesa com hora formatada no firebase

--- app/src/main/java/com/example/organizze/activity/DespesasActivity.java
+++ app/src/main/java/com/example/organizze/activity/DespesasActivity.java
@@ -3,16 +3,19 @@ package com.example.organizze.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 
 import com.example.organizze.R;
 import com.example.organizze.activity.helper.DataUtil;
+import com.example.organizze.activity.model.Movimentacao;
 import com.google.android.material.textfield.TextInputEditText;
 
 public class DespesasActivity extends AppCompatActivity {
 
     private TextInputEditText campoData, campoCategoria, camporDescricao;
     private EditText campoValor;
+    private Movimentacao movimentacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +28,16 @@ public class DespesasActivity extends AppCompatActivity {
 
         //Preenche o campo data com a data atual
         campoData.setText(DataUtil.dataAtual());
+    }
+
+    public void salvarDespesa(View view){
+        movimentacao = new Movimentacao();
+        String data = campoData.getText().toString();
+        movimentacao.setValor(Double.parseDouble(campoValor.getText().toString()));
+        movimentacao.setCategoria(campoCategoria.getText().toString());
+        movimentacao.setDescricao(camporDescricao.getText().toString());
+        movimentacao.setData(data);
+        movimentacao.setTipo("d");
+        movimentacao.salvar(data);
     }
 }
--- app/src/main/java/com/example/organizze/activity/helper/DataUtil.java
+++ app/src/main/java/com/example/organizze/activity/helper/DataUtil.java
@@ -12,4 +12,14 @@ public class DataUtil {
         return dataFormat;
     }
 
+    public static String mesAnoDataEscolhida(String data){
+        String retornoData[] = data.split("/");
+        String dia = retornoData[0];
+        String mes = retornoData[1];
+        String ano = retornoData[2];
+
+        String mesAno = mes + ano;
+        return mesAno;
+    }
+
 }
